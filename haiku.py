f3d71a56b5dbd46535bad587664fc4180b0199dc
Cate Uselton <cathode.use@gmail.com>
2015-01-01 14:07:46 -0800

    builds a line with a minimum syllable count

--- haiku.py
+++ haiku.py
@@ -1,17 +1,30 @@
 import nltk
 import random
 from nltk.corpus import cmudict
+d = cmudict.dict()
 
 def hasNumber(strInput):
     return any(char.isdigit() for char in strInput)
 
-d = cmudict.dict()
-w1 = random.choice(cmudict.words())
-syllableCount = 0
-print w1
-for x in range(0, len(d[w1][0])):
-    # print d[w1][0][x]
-    if hasNumber(d[w1][0][x]):
-        syllableCount += 1
+def countSyllables(phonemeList):
+    syllableCount = 0
+    for x in range(0, len(phonemeList)):
+        if hasNumber(phonemeList[x]):
+            syllableCount += 1
+    return syllableCount
 
-print syllableCount
+def buildLine(totalSyllables):
+    line = ""
+    lineSyllables = 0
+    while lineSyllables < totalSyllables:
+        word = random.choice(cmudict.words())
+        line = addWord(line, word)
+        lineSyllables += countSyllables(d[word][0])
+    return line
+
+def addWord(line, word):
+    line += word + " "
+    return line
+
+# line1 = buildLine(5)
+print buildLine(5)
